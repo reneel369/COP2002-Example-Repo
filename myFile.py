460d256ab5370d150591a11ddaf080cf14e6cfbd
reneel369 <renee.lunan@gmail.com>
2025-05-24 10:44:32 -0400

    Add files via upload

---
+++ myFile.py
@@ -0,0 +1,7 @@
+Python 3.13.3 (v3.13.3:6280bb54784, Apr  8 2025, 10:47:54) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> # Creating a project for GitHub
+>>> # By Renee Lunan
+>>> 
+>>> print("COP2002 FALL 2030")
+COP2002 FALL 2030
